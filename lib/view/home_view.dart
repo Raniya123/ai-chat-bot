96280e0f2ef96afc4b72d7d44cef082eb0977ef2
Rania benOtman <rawan_ben_2002@yahoo.com>
2024-06-01 22:13:41 +0200

    Update home_view.dart

--- lib/view/home_view.dart
+++ lib/view/home_view.dart
@@ -13,109 +13,113 @@ class HomeView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppThemes.mainAppBar("AI - CHATBOT"),
-      body: Container(
-        padding: const EdgeInsets.symmetric(vertical: 50, horizontal: 20),
-        child: Column(
-          verticalDirection: VerticalDirection.up,
-          children: [
-            Container(
-              padding: const EdgeInsets.symmetric(horizontal: 10),
-              decoration: BoxDecoration(
-                color: Colors.white,
-                border: Border.all(color: Colors.black),
-              ),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Expanded(
-                    flex: 5,
-                    child: Center(
-                      child: TextField(
-                        controller: _chat.userChatController,
-                        onChanged: (value) {
-                          if (kDebugMode) {
-                            print(value);
-                          }
-                        },
-                        decoration: const InputDecoration(
-                          floatingLabelBehavior: FloatingLabelBehavior.never,
-                          border: InputBorder.none,
-                          labelText: 'Enter text',
+    return SafeArea(
+      bottom: true,
+      top: false,
+      child: Scaffold(
+        appBar: AppThemes.mainAppBar("AI - CHATBOT"),
+        body: Container(
+          padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 20),
+          child: Column(
+            verticalDirection: VerticalDirection.up,
+            children: [
+              Container(
+                padding: const EdgeInsets.symmetric(horizontal: 10),
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  border: Border.all(color: Colors.black),
+                ),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Expanded(
+                      flex: 5,
+                      child: Center(
+                        child: TextField(
+                          controller: _chat.userChatController,
+                          onChanged: (value) {
+                            if (kDebugMode) {
+                              print(value);
+                            }
+                          },
+                          decoration: const InputDecoration(
+                            floatingLabelBehavior: FloatingLabelBehavior.never,
+                            border: InputBorder.none,
+                            labelText: 'Enter text',
+                          ),
                         ),
                       ),
                     ),
-                  ),
-                  Expanded(
-                      child: GestureDetector(
-                    onTap: () {
-                      _chat.sendMessageToGemini(
-                          "me", _chat.userChatController.text);
-                    },
-                    child: Container(
-                      padding: const EdgeInsets.all(10),
-                      decoration: BoxDecoration(
-                        color: AppThemes.greenColor.withOpacity(0.6),
-                        border: Border.all(color: Colors.black),
-                      ),
-                      child: const Text(
-                        "ADD",
-                        textAlign: TextAlign.center,
-                      ),
-                    ),
-                  )),
-                ],
-              ),
-            ),
-            Obx(
-              () => _chat.isLoading.value
-                  ? Lottie.asset(
-                      fit: BoxFit.fitHeight,
-                      Assets.loading,
-                      height: 60,
-                    )
-                  : const SizedBox(
-                      height: 20,
-                    ),
-            ),
-            Expanded(
-                child: Obx(() => ListView.builder(
-                    padding: const EdgeInsets.all(0),
-                    reverse: true, // to add new items from bottom to top
-                    itemCount: _chat.messages.length,
-                    itemBuilder: (context, index) {
-                      return Container(
+                    Expanded(
+                        child: GestureDetector(
+                      onTap: () {
+                        _chat.sendMessageToGemini(
+                            "me", _chat.userChatController.text);
+                      },
+                      child: Container(
                         padding: const EdgeInsets.all(10),
-                        margin: const EdgeInsets.only(top: 10),
                         decoration: BoxDecoration(
-                          color: index % 2 == 0
-                              ? AppThemes.orgColor
-                              : AppThemes.greenColor,
-                          border: Border.all(width: 0),
+                          color: AppThemes.greenColor.withOpacity(0.6),
+                          border: Border.all(color: Colors.black),
                         ),
-                        child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Text(
-                              _chat.messages[index].senderName,
-                              maxLines: 10,
-                              style: const TextStyle(
-                                  fontSize: 12, fontWeight: FontWeight.w900),
-                            ),
-                            Padding(
-                              padding:
-                                  const EdgeInsets.symmetric(vertical: 8.0),
-                              child: MarkdownBody(
-                                selectable: true,
-                                data: _chat.messages[index].body,
+                        child: const Text(
+                          "ADD",
+                          textAlign: TextAlign.center,
+                        ),
+                      ),
+                    )),
+                  ],
+                ),
+              ),
+              Obx(
+                () => _chat.isLoading.value
+                    ? Lottie.asset(
+                        fit: BoxFit.fitHeight,
+                        Assets.loading,
+                        height: 60,
+                      )
+                    : const SizedBox(
+                        height: 20,
+                      ),
+              ),
+              Expanded(
+                  child: Obx(() => ListView.builder(
+                      padding: const EdgeInsets.all(0),
+                      reverse: true, // to add new items from bottom to top
+                      itemCount: _chat.messages.length,
+                      itemBuilder: (context, index) {
+                        return Container(
+                          padding: const EdgeInsets.all(10),
+                          margin: const EdgeInsets.only(top: 10),
+                          decoration: BoxDecoration(
+                            color: index % 2 == 0
+                                ? AppThemes.orgColor
+                                : AppThemes.greenColor,
+                            border: Border.all(width: 0),
+                          ),
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(
+                                _chat.messages[index].senderName,
+                                maxLines: 10,
+                                style: const TextStyle(
+                                    fontSize: 12, fontWeight: FontWeight.w900),
                               ),
-                            )
-                          ],
-                        ),
-                      );
-                    })))
-          ],
+                              Padding(
+                                padding:
+                                    const EdgeInsets.symmetric(vertical: 8.0),
+                                child: MarkdownBody(
+                                  selectable: true,
+                                  data: _chat.messages[index].body,
+                                ),
+                              )
+                            ],
+                          ),
+                        );
+                      })))
+            ],
+          ),
         ),
       ),
     );
