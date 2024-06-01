02c2d68225948b7102eeb422f91e3b33af9ef751
Rania benOtman <rawan_ben_2002@yahoo.com>
2024-06-01 22:20:50 +0200

    new improvment

--- lib/view/home_view.dart
+++ lib/view/home_view.dart
@@ -1,14 +1,15 @@
-import 'package:ai_chat_bot/controller/chat_controller.dart';
-import 'package:ai_chat_bot/theme/assets.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-import 'package:ai_chat_bot/theme/theme.dart';
 import 'package:get/get.dart';
 import 'package:flutter_markdown/flutter_markdown.dart';
 import 'package:lottie/lottie.dart';
+import '../controller/chat_controller.dart';
+import '../theme/assets.dart';
+import '../theme/theme.dart';
 
 class HomeView extends StatelessWidget {
   HomeView({super.key});
+
   final _chat = Get.put(ChatController());
 
   @override
@@ -51,30 +52,30 @@ class HomeView extends StatelessWidget {
                       ),
                     ),
                     Expanded(
-                        child: GestureDetector(
-                      onTap: () {
-                        _chat.sendMessageToGemini(
-                            "me", _chat.userChatController.text);
-                      },
-                      child: Container(
-                        padding: const EdgeInsets.all(10),
-                        decoration: BoxDecoration(
-                          color: AppThemes.greenColor.withOpacity(0.6),
-                          border: Border.all(color: Colors.black),
-                        ),
-                        child: const Text(
-                          "ADD",
-                          textAlign: TextAlign.center,
+                      child: GestureDetector(
+                        onTap: () {
+                          _chat.sendMessageToGemini(
+                              "me", _chat.userChatController.text);
+                        },
+                        child: Container(
+                          padding: const EdgeInsets.all(10),
+                          decoration: BoxDecoration(
+                            color: AppThemes.greenColor.withOpacity(0.6),
+                            border: Border.all(color: Colors.black),
+                          ),
+                          child: const Text(
+                            "ADD",
+                            textAlign: TextAlign.center,
+                          ),
                         ),
                       ),
-                    )),
+                    ),
                   ],
                 ),
               ),
               Obx(
                 () => _chat.isLoading.value
                     ? Lottie.asset(
-                        fit: BoxFit.fitHeight,
                         Assets.loading,
                         height: 60,
                       )
@@ -83,41 +84,45 @@ class HomeView extends StatelessWidget {
                       ),
               ),
               Expanded(
-                  child: Obx(() => ListView.builder(
-                      padding: const EdgeInsets.all(0),
-                      reverse: true, // to add new items from bottom to top
-                      itemCount: _chat.messages.length,
-                      itemBuilder: (context, index) {
-                        return Container(
-                          padding: const EdgeInsets.all(10),
-                          margin: const EdgeInsets.only(top: 10),
-                          decoration: BoxDecoration(
-                            color: index % 2 == 0
-                                ? AppThemes.orgColor
-                                : AppThemes.greenColor,
-                            border: Border.all(width: 0),
-                          ),
-                          child: Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              Text(
-                                _chat.messages[index].senderName,
-                                maxLines: 10,
-                                style: const TextStyle(
-                                    fontSize: 12, fontWeight: FontWeight.w900),
+                child: Obx(
+                  () => ListView.builder(
+                    padding: const EdgeInsets.all(0),
+                    reverse: true,
+                    itemCount: _chat.messages.length,
+                    itemBuilder: (context, index) {
+                      return Container(
+                        padding: const EdgeInsets.all(10),
+                        margin: const EdgeInsets.only(top: 10),
+                        decoration: BoxDecoration(
+                          color: index % 2 == 0
+                              ? AppThemes.orgColor
+                              : AppThemes.greenColor,
+                          border: Border.all(width: 0),
+                        ),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              _chat.messages[index].senderName,
+                              maxLines: 10,
+                              style: const TextStyle(
+                                  fontSize: 12, fontWeight: FontWeight.w900),
+                            ),
+                            Padding(
+                              padding:
+                                  const EdgeInsets.symmetric(vertical: 8.0),
+                              child: MarkdownBody(
+                                selectable: true,
+                                data: _chat.messages[index].body,
                               ),
-                              Padding(
-                                padding:
-                                    const EdgeInsets.symmetric(vertical: 8.0),
-                                child: MarkdownBody(
-                                  selectable: true,
-                                  data: _chat.messages[index].body,
-                                ),
-                              )
-                            ],
-                          ),
-                        );
-                      })))
+                            ),
+                          ],
+                        ),
+                      );
+                    },
+                  ),
+                ),
+              ),
             ],
           ),
         ),
