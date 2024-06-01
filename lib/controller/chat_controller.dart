24462916eb9297c81ec74fc9145986f1ec067404
Rania benOtman <rawan_ben_2002@yahoo.com>
2024-06-01 22:18:37 +0200

    Update chat_controller.dart

--- lib/controller/chat_controller.dart
+++ lib/controller/chat_controller.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_gemini/flutter_gemini.dart';
 import 'package:get/get.dart';
@@ -6,11 +7,12 @@ import '../model/model.dart';
 class ChatController extends GetxController {
   final _messages = <Message>[].obs;
   List<Message> get messages => _messages;
-  final gemini = Gemini.instance;
-  final geminiRsp = ''.obs;
+  final Gemini gemini = Gemini.instance;
+  final geminiResponse = ''.obs;
   final isLoading = false.obs;
 
   final TextEditingController userChatController = TextEditingController();
+
   void addMessage(String senderName, String body) {
     final message = Message(
       senderName: senderName,
@@ -22,25 +24,39 @@ class ChatController extends GetxController {
 
   Future<void> sendMessageToGemini(
       String senderName, String messageText) async {
-    isLoading.value = true;
-    userChatController.clear();
-    // Add the user's message
-    addMessage(senderName, messageText);
+    try {
+      isLoading.value = true;
+      userChatController.clear();
 
-    // Simulate sending the message to Gemini AI and receiving a response
-    final responseText = await getGeminiResponse(messageText);
+      // Add the user's message
+      addMessage(senderName, messageText);
 
-    // Add Gemini AI's response
-    addMessage('Gemini AI', responseText);
-    isLoading.value = false;
-    update();
+      // Simulate sending the message to Gemini AI and receiving a response
+      final responseText = await getGeminiResponse(messageText);
+
+      // Add Gemini AI's response
+      addMessage('Gemini AI', responseText);
+    } catch (e) {
+      // Handle errors gracefully
+      if (kDebugMode) {
+        print('Error: $e');
+      }
+    } finally {
+      isLoading.value = false;
+      update();
+    }
   }
 
   Future<String> getGeminiResponse(String messageText) async {
-    await gemini
-        .text(messageText)
-        .then((value) => geminiRsp.value = value!.output ?? "No responce")
-        .catchError((e) => throw e);
-    return geminiRsp.value;
+    try {
+      final response = await gemini.text(messageText);
+      return response?.output ?? "No response";
+    } catch (e) {
+      // Handle errors gracefully
+      if (kDebugMode) {
+        print('Error getting response from Gemini: $e');
+      }
+      return "Error";
+    }
   }
 }
